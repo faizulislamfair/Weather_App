63d0876d239e8e9e982bc41c47bfe67cff50442a
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-05-18 23:40:02 +0600

    weather conditions

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -10,18 +10,70 @@ import Weather from './app_component/weather.component'
 const API_key="543131be8589bf754ffee80adfb1bebc"
 
 class App extends React.Component {
-  state = { }
+
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min:undefined,
+      description:"",
+      error:false
+    };
+    this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
+    }
+  }
+
+  calCelsius(temp){
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+       city: response.name,
+       country: response.sys.country,
+       celsius: this.calCelsius(response.main.temp),
+       temp_max: this.calCelsius(response.main.temp_max),
+       temp_min: this.calCelsius(response.main.temp_min),
+       description: response.weather[0].description,
+       icon:this.weatherIcon.Thunderstorm
+    })
+  }
+   
+
   render(){
-    return();
+    return(
+      <div className="App">
+        <Weather city={this.state.city} country={this.state.country} temp_celsius={this.state.celsius} 
+        temp_max={this.state.temp_max}
+        temp_min={this.state.temp_min}
+        description={this.state.description}
+        weatherIcon={this.state.icon}
+        />
+      </div>
+    );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Weather/>
-    </div>
-  );
-}
-
 export default App;
+
